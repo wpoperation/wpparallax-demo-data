3cbe7aefffab1d7a3372d85111e1209273705f1f
sagar <girisagar051@gmail.com>
2019-07-02 22:22:27 +0545

    added config.php

---
+++ config.php
@@ -0,0 +1,202 @@
+<?php
+
+      $data = array(
+        'default' => array(
+          'categories'        => array( 'Default','Agency','Business' ),
+          'preview_url'     => 'https://demo.wpoperation.com/wpparallax/default/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '10',
+          'is_shop' => true,
+          'menus'         => array(
+              'menu-1' => 'Menu 1'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'contact-form-7',
+                'init'    => 'contact-form-7/wp-contact-form-7.php',
+                'name'    => 'Contact Form 7',
+              ),
+              array(
+                'slug'    => 'woocommerce',
+                'init'    => 'woocommerce/woocommerce.php',
+                'name'    => 'WooCommerce',
+              ),
+            ),
+          ),
+        ),
+        'education' => array(
+          'categories'        => array( 'Education','Agency' ),
+          'preview_url'     => 'https://demo.wpoperation.com/wpparallax/education/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '10',
+          'is_shop' => false,
+          'menus'         => array(
+              'menu-1' => 'Menu 1'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'elementor',
+                'init'    => 'elementor/elementor.php',
+                'name'    => 'Elementor',
+              ),
+              array(
+                'slug'    => 'essential-addons-for-elementor-lite',
+                'init'    => 'essential-addons-for-elementor-lite/essential-addons-for-elementor-lite.php',
+                'name'    => 'Elementor Essential Addons',
+              ),
+              array(
+                'slug'    => 'contact-form-7',
+                'init'    => 'contact-form-7/wp-contact-form-7.php',
+                'name'    => 'Contact Form 7',
+              ),
+            ),
+          ),
+        ),
+        'medical' => array(
+          'categories'        => array( 'Medical' ),
+          'preview_url'     => 'https://demo.wpoperation.com/wpparallax/medical/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '10',
+          'is_shop' => false,
+          'menus'         => array(
+              'menu-1' => 'Menu 1'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'elementor',
+                'init'    => 'elementor/elementor.php',
+                'name'    => 'Elementor',
+              ),
+              array(
+                'slug'    => 'essential-addons-for-elementor-lite',
+                'init'    => 'essential-addons-for-elementor-lite/essential-addons-for-elementor-lite.php',
+                'name'    => 'Elementor Essential Addons',
+              ),
+              array(
+                'slug'    => 'contact-form-7',
+                'init'    => 'contact-form-7/wp-contact-form-7.php',
+                'name'    => 'Contact Form 7',
+              ),
+            ),
+          ),
+        ),
+        'construction' => array(
+          'categories'        => array( 'Construction','Agency' ),
+          'preview_url'     => 'https://demo.wpoperation.com/wpparallax/construction/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '10',
+          'is_shop' => true,
+          'menus'         => array(
+              'menu-1' => 'Menu 1'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'elementor',
+                'init'    => 'elementor/elementor.php',
+                'name'    => 'Elementor',
+              ),
+              array(
+                'slug'    => 'essential-addons-for-elementor-lite',
+                'init'    => 'essential-addons-for-elementor-lite/essential-addons-for-elementor-lite.php',
+                'name'    => 'Elementor Essential Addons',
+              ),
+              array(
+                'slug'    => 'contact-form-7',
+                'init'    => 'contact-form-7/wp-contact-form-7.php',
+                'name'    => 'Contact Form 7',
+              ),
+              array(
+                'slug'    => 'woocommerce',
+                'init'    => 'woocommerce/woocommerce.php',
+                'name'    => 'WooCommerce',
+              ),
+            ),
+          ),
+        ),
+        'restaurant' => array(
+          'categories'        => array( 'Restaurant','Business' ),
+          'preview_url'     => 'https://demo.wpoperation.com/wpparallax/restaurant/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '10',
+          'is_shop' => false,
+          'menus'         => array(
+              'menu-1' => 'Main Menu'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'elementor',
+                'init'    => 'elementor/elementor.php',
+                'name'    => 'Elementor',
+              ),
+              array(
+                'slug'    => 'essential-addons-for-elementor-lite',
+                'init'    => 'essential-addons-for-elementor-lite/essential-addons-for-elementor-lite.php',
+                'name'    => 'Elementor Essential Addons',
+              ),
+              array(
+                'slug'    => 'contact-form-7',
+                'init'    => 'contact-form-7/wp-contact-form-7.php',
+                'name'    => 'Contact Form 7',
+              ),
+            ),
+          ),
+        ),
+        'business' => array(
+          'categories'        => array( 'Agency','Business' ),
+          'preview_url'     => 'https://demo.wpoperation.com/wpparallax/business/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '10',
+          'is_shop' => false,
+          'menus'         => array(
+              'menu-1' => 'Menu 1'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'elementor',
+                'init'    => 'elementor/elementor.php',
+                'name'    => 'Elementor',
+              ),
+              array(
+                'slug'    => 'essential-addons-for-elementor-lite',
+                'init'    => 'essential-addons-for-elementor-lite/essential-addons-for-elementor-lite.php',
+                'name'    => 'Elementor Essential Addons',
+              ),
+              array(
+                'slug'    => 'contact-form-7',
+                'init'    => 'contact-form-7/wp-contact-form-7.php',
+                'name'    => 'Contact Form 7',
+              ),
+            ),
+          ),
+        ),
+      );
